fff5d6c4e0593f3351f7fcb0fb403e9ce184bf84
Andrey Mironov <Dehta-Hun>
2023-12-03 22:39:58 +0300

    nextgen

--- lua/mironovak/remap.lua
+++ lua/mironovak/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<A-n>", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeFocus)
--- lua/mironovak/set.lua
+++ lua/mironovak/set.lua
@@ -1,11 +1,5 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
--- vim.notify = require("notify")
-
--- vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- vim.opt.foldlevelstart = 3
--- little space
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -36,24 +30,25 @@ vim.opt.splitright = true
 vim.opt.undofile = true
 
 vim.opt.cursorline = true
-vim.cmd('let g:rnvimr_enable_picker = 1')
+vim.cmd("let g:rnvimr_enable_picker = 1")
 
 vim.filetype.add({
-    extension = {
-       bpt = 'xml',
-    },
-    filename = {
-    },
-    pattern = {
-        [ '.*ansible(.*/?)(.*/?)(.*/?)(.*/?)%w*.ya?ml' ] = 'yaml.ansible',
-    }
+	extension = {
+		bpt = "xml",
+	},
+	filename = {},
+	pattern = {
+		[".*ansible(.*/?)(.*/?)(.*/?)(.*/?)%w*.ya?ml"] = "yaml.ansible",
+	},
 })
 
-vim.api.nvim_create_autocmd('BufWritePost', {
-    callback = function()
-        local path = vim.api.nvim_buf_get_name(0):match('.*projectsGit(.*/?)(.*/?)(.*/?)(.*/?)(.*/?)(.*/?)(.*/?)%w*.$')
-        if path then
-            os.execute('tmux new-session -d "rsync -aP ~/projectsGit/Corp-FWaaS qc-fwaas-manual-kvm-mironov.avp.ru:/home/mironov_a/ > /dev/null 2>&1"')
-        end
-    end
+vim.api.nvim_create_autocmd("BufWritePost", {
+	callback = function()
+		local path = vim.api.nvim_buf_get_name(0):match(".*projectsGit(.*/?)(.*/?)(.*/?)(.*/?)(.*/?)(.*/?)(.*/?)%w*.$")
+		if path then
+			os.execute(
+				'tmux new-session -d "rsync -aP ~/projectsGit/Corp-FWaaS qc-fwaas-manual-kvm-mironov.avp.ru:/home/mironov_a/ > /dev/null 2>&1"'
+			)
+		end
+	end,
 })
