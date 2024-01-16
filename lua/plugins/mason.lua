098a928bce2714dffefe23334827850cbe97e521
Mironov Andrey <andrey.mironov@kaspersky.com>
2024-01-16 19:07:35 +0300

    moving to null-ls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,29 +1,35 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"ansiblels",
-					"bash-language-server",
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "ansiblels",
                     "ruff_lsp",
-					"lua_ls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			-- lspconfig.lua_ls.setup({})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+                    "lua_ls",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.ruff_lsp.setup({
+                init_options = {
+                    workspace = {
+                        extraPaths = { "/home/mironov_a/projectsGit/ci-test-scenarios/" },
+                    },
+                },
+            })
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,30 @@
+return {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        local utils = require("null-ls.utils")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.diagnostics.ruff,
+                null_ls.builtins.diagnostics.mypy,
+                -- null_ls.builtins.diagnostics.mypy.with({
+                --     runtime_condition = function(params)
+                --         return utils.path.exists(params.bufname)
+                --     end,
+                --     extra_args = {"--config-file", "/home/mironov_a/.config/mypy.ini"},
+                --     cwd = function () return vim.fn.getcwd() end
+                -- }),
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.ruff.with({
+                    "--config",
+                    "$HOME/.config/ruff.toml",
+                }),
+                null_ls.builtins.formatting.ruff_format.with({
+                    "--config",
+                    "$HOME/.config/ruff.toml",
+                }),
+            },
+        })
+        vim.keymap.set("n", "gq", vim.lsp.buf.format, {})
+    end,
+}
