f09f8ae52ce830fe538267ee5f1dc656ad571f7b
Mironov Andrey <Andrey.Mironov@kaspersky.com>
2024-01-18 19:15:50 +0300

    fix formatting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -21,11 +21,11 @@ return {
 						-- Shows shortened relative path when set to false.
 						show_filename_only = true,
 						-- Hide filename extension when set to true.
-						hide_filename_extension = false,
+						hide_filename_extension = true,
 						-- Shows indicator when the buffer is modified.
 						show_modified_status = true,
 
-						mode = 0, -- 0: Shows buffer name
+						mode = 2, -- 0: Shows buffer name
 						-- 1: Shows buffer index
 						-- 2: Shows buffer name + buffer index
 						-- 3: Shows buffer number
@@ -34,11 +34,11 @@ return {
 						filetype_names = {
 							TelescopePrompt = "Telescope",
 							dashboard = "Dashboard",
-							packer = "Packer",
+							lazy = "Lazy",
 							fzf = "FZF",
 							alpha = "Alpha",
 						}, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
-						use_mode_colors = false,
+						use_mode_colors = true,
 
 						symbols = {
 							-- Text to show when the buffer is modified
