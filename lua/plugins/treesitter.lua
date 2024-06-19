4884da3794acda0572f10e333d98c5e959388921
Mironov Andrey <andrey.mironov@kaspersky.com>
2024-06-19 15:46:46 +0300

    formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,26 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {"lua", "vim", "vimdoc", "query", "heex", "javascript", "html", "python", "xml", "json", },
-            sync_install = true,
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+		configs.setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"heex",
+				"javascript",
+				"html",
+				"python",
+				"xml",
+				"json",
+			},
+			sync_install = true,
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
--- "nvim-treesitter/nvim-treesitter",
--- lazy = false,
--- config = function()
---     require("nvim-treesitter.configs").setup({
---         ensure_installed = "all",
---         sync_install = true,
---         auto_install = true,
---         ignore_install = {},
---         highlight = {
---             enable = true,
---         },
---     })
--- end,
