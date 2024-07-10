77d76f5bdc71d2a811a4f3d3562fa99e2e58d44d
Mironov Andrey <andrey.mironov@kaspersky.com>
2024-07-10 18:07:53 +0300

    disable update notifications

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -67,20 +67,20 @@ return {
 		require("lspconfig")["lua_ls"].setup({
 			capabilities = capabilities,
 		})
-		-- require("lspconfig")["ruff_lsp"].setup({
-		-- 	capabilities = capabilities,
-		-- 	on_attach = function(client, bufnr)
-		-- 		if client.supports_method("textDocument/formatting") then
-		-- 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		-- 			vim.api.nvim_create_autocmd("BufWritePre", {
-		-- 				group = augroup,
-		-- 				buffer = bufnr,
-		-- 				callback = function()
-		-- 					vim.lsp.buf.format()
-		-- 				end,
-		-- 			})
-		-- 		end
-		-- 	end,
-		-- })
+		require("lspconfig")["ruff_lsp"].setup({
+			capabilities = capabilities,
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format()
+						end,
+					})
+				end
+			end,
+		})
 	end,
 }
