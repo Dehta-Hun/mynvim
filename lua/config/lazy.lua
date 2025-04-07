3731d6645ceef39d799fbbdd0918dd636c745924
Andrey Mironov <Andrey.Mironov@kaspersky.com>
2025-04-07 19:52:47 +0300

    somefix

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,9 +29,9 @@ require("lazy").setup({
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "habamax" } },
+	install = { colorscheme = { "catppuccin" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = true, frequency = 86400 },
 })
 local function augroup(name)
 	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,6 +1,6 @@
 return {
 	"ThePrimeagen/harpoon",
-	-- lazy = true
+	-- lazy = true,
 	config = function()
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
