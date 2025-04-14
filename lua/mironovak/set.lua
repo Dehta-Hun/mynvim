79bd4ac8285428965a44cac746118e7f7052b614
Andrey Mironov <Andrey.Mironov@kaspersky.com>
2025-04-14 18:20:39 +0300

    rsync fix

--- lua/mironovak/set.lua
+++ lua/mironovak/set.lua
@@ -50,10 +50,10 @@ vim.filetype.add({
 -- RSync repo to testmachine
 vim.api.nvim_create_autocmd("BufWritePost", {
 	callback = function()
-		local path = vim.api.nvim_buf_get_name(0):match(".*projectsGit(.*/?)")
+		local path = vim.api.nvim_buf_get_name(0): match(".*Corp.FWaaS.*")
 		if path then
 			os.execute(
-				'tmux new-session -d "rsync -aP ~/projectsGit/Corp-FWaaS qc-fwaas-manual-kvm-mironov-2.avp.ru: > /dev/null 2>&1"'
+				'tmux new-session -d "rsync -aP ~/Corp-FWaaS qc-fwaas-manual-kvm-mironov-2.avp.ru: > /dev/null 2>&1"'
 			)
 		end
 	end,
